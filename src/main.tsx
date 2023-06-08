29b8d4a19fb0a5ed727518c745c0849901dc16ff
Abdjalil Brihoum <abdjalil@brihoum.com>
2023-06-08 14:52:50 +0100

    rename folders

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import FormReactFormHook from "./form-react-form-hook/FormReactFormHook";
+import FormReactFormHook from "./form-with-react-form-hook/FormReactFormHook";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <FormReactFormHook />
